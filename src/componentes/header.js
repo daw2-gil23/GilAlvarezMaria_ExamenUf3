d1fb8e0af9c59b9dc52da0517b4f2b2effc20a1f
daw2-gil23 <gilalvarezmaria@fpllefia.com>
2023-03-10 15:42:24 +0100

    header pt.3

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -1,14 +1,7 @@
 
-<<<<<<< HEAD
 export const header ={
     template: ` <h1 class="pt-2">Maria Gil Alvarez</h1> `,
     script: ()=>{
         console.log('hola soy header')
-=======
-export const beader ={
-    template: ` <p>Maria Gil Alvarez</p> `,
-    script: ()=>{
-        console.log('hola soy Home')
->>>>>>> a01fe23c036cdd8e9349ebc686129cc731f7bdde
     }
 }
